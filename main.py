b1bf398177ce034e79f6c45ba6797ee145d5a29c
IwoMich <iwonamichalczyk@onet.eu>
2026-04-20 19:33:06 +0200

    save to mysql

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 from services.openweather_api import get_weather
 from services.excel_files import save_to_excel, read_excel
 from services.dashboard import render_dashboard
-from services.mysql_db import create_db_and_table, save_to_mysql
+from services.mysql_db import create_db_and_table, save_to_mysql, get_all_weather
 import time
 
 create_db_and_table()
+
+# results_excel = read_excel("weather_data.xlsx")
+# results_sql = get_all_weather()
+# for r in results_sql:
+#     print(r)
 
 while True:
     weather = get_weather()
@@ -12,6 +17,8 @@ while True:
     save_to_mysql(weather)
     print("Pobieram i zapisuję dane")
     time.sleep(5)
+
+# render_dashboard("lisbon.xlsx")
 
 
 
